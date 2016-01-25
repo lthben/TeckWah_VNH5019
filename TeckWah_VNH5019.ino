24ee96d11807fea72498dea099f91ab3fe90599c
Benjamin Low <lthben@gmail.com>
2016-01-25 15:57:57 +0800

    commit

--- TeckWah_VNH5019.ino
+++ TeckWah_VNH5019.ino
@@ -4,19 +4,23 @@
  * controls a pair of linear actuators. Documentation found at
  * https://www.pololu.com/product/2507
  *
- * Last updated: 28 Dec 2015
+ * Last updated: 26 Jan 2016
  */
 
 #include "DualVNH5019MotorShield.h"
 
+//USER DEFINED SETTINGS
+bool DEBUG = false;
+int thisLinearActuatorBoxNum = 2; //1 is for the horizontal drawer, 2 for the vertical display
+
 DualVNH5019MotorShield md;
 enum motor_states { extend, retract, neutral };
 motor_states first_motor_state = neutral;
-motor_states second_motor_state = neutral;
+//motor_states second_motor_state = neutral;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Dual VNH5019 Motor Shield");
+  if (DEBUG) Serial.println("Dual VNH5019 Motor Shield");
   md.init();
 }
 
@@ -26,10 +30,10 @@ void loop() {
   
   switch (first_motor_state) {
     case (extend):
-      md.setM1Speed(-400);
+        md.setM1Speed(-400);
       break;
     case (retract):
-      md.setM1Speed(400);
+        md.setM1Speed(400);
       break;
     case (neutral):
       md.setM1Brake(400);
@@ -38,19 +42,20 @@ void loop() {
       break;
   }
 
+    /*
   switch (second_motor_state) {
     case (extend):
-      md.setM2Speed(-400);
+        md.setM2Speed(-400);
       break;
     case (retract):
-      md.setM2Speed(400);
+        md.setM2Speed(400);
       break;
     case (neutral):
       md.setM2Brake(400);
       break;
     default:
       break;
-  }
+  } */
 }
 
 void serialEvent() {
@@ -60,64 +65,26 @@ void serialEvent() {
   if (incoming == '1') {
 
     first_motor_state = extend;
-    Serial.println("extending first");
+    if (DEBUG)   Serial.println("extending");
 
-  } else if (incoming == '2') {
-
-    first_motor_state = retract;
-    Serial.println("retracting first");
-
-  } else if (incoming == '3') {
-
-    first_motor_state = neutral;
-    Serial.println("stopping first");
-
-  } else if (incoming == '4') {
-
-    second_motor_state = extend;
-    Serial.println("extending second");
-
-  } else if (incoming == '5') {
-
-    second_motor_state = retract;
-    Serial.println("retracting second");
-
-  } else if (incoming == '6') {
-
-    second_motor_state = neutral;
-    Serial.println("stopping second");
-
-  } else if (incoming == '7') {
-
-    first_motor_state = extend;
-    second_motor_state = extend;
-    Serial.println("extending both");
-
-  } else if (incoming == '8') {
+  } else if (incoming == '0') {
 
     first_motor_state = retract;
-    second_motor_state = retract;
-    Serial.println("retracting both");
-
-  } else if (incoming == '9') {
-
-    first_motor_state = neutral;
-    second_motor_state = neutral;
-    Serial.println("stopping both");
-  }
+    if (DEBUG) Serial.println("retracting");
+  } 
 }
 
 void stopIfFault()
 {
   if (md.getM1Fault())
   {
-    Serial.println("M1 fault");
+    if (DEBUG) Serial.println("M1 fault");
     while (1);
-  }
+  } /*
   if (md.getM2Fault())
   {
-    Serial.println("M2 fault");
+    if (DEBUG) Serial.println("M2 fault");
     while (1);
-  }
+  } */
 }
 
